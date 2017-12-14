651956ea6db585f621e9be2badb7c3633b92e186
Farhan Sabran <FarhanMS123@users.noreply.github.com>
2017-12-14 22:25:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var net = require("net");
 
-var c = net.createServer(); //.listen(80);
+var c = net.createServer().listen(9999);
 
 c.on("connection", function(c){
   c.write("HTTP/1.1 200 OK\r\nAccess-Control-Allow-Origin: *\r\nContent-Type: text/html\r\n\r\n<h1>Hello world!</h1>");

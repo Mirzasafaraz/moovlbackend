f8703e2377b2550d414ba870f7511cca425f0485
Farhan Sabran <FarhanMS123@users.noreply.github.com>
2017-12-14 11:00:54 +0700

    Add files via upload

---
+++ handler.js
@@ -0,0 +1,38 @@
+var cp = require("child_process");
+var net = require("net");;
+var path = require("path");
+var os = require("os")
+
+global.c = cp.spawn("node", ["console.js"]);
+
+setTimeout(function(){
+	global.s = net.createConnection(path.join('\\\\?\\pipe', process.cwd(), 'consoleHandler'));
+	s.on("error", function(e){
+		console.log(`error : ${os.EOL}${e}`)
+	}).on("connect", function(){
+		console.log("connected");
+	});
+},1000);
+
+c.stdout.on("data", function(dt){
+	dt = dt.toString();
+	s.write(dt);
+	console.log(dt)
+});
+c.on("exit", function(){
+	global.c = cp.spawn("node", ["console.js"]);
+	setTimeout(function(){
+		global.s = net.createConnection(path.join('\\\\?\\pipe', process.cwd(), 'consoleHandler'));
+		s.on("error", function(e){
+			console.log(`error : ${os.EOL}${e}`)
+		}).on("connect", function(){
+			console.log("connected");
+		});
+	},1000);
+
+	c.stdout.on("data", function(dt){
+		dt = dt.toString();
+		s.write(dt);
+		process.stdout.write(dt);
+	});
+})

d9bef7927dd296bb4775660bbdbf03b0867f60fb
Farhan Sabran <FarhanMS123@users.noreply.github.com>
2017-12-17 01:55:07 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+var net = require("net");
+var cp = require("child_process");
+var html = require("./html");
+var njs = require("./njs");
+
+var log = []; //[dt[, s], time, type]; type=object[_event]
+
+var c = net.createServer().listen(process.opt.PORT);
+
+log.push([process.opt.PORT, (new Date()).getTime(), "serv_listen"]);
+
+s.on("connection", function(c){
+	log.push(["", c, (new Date()).getTime(), "conn"]);
+	c.on("data", function(dt){
+		log.push([dt, c, (new Date()).getTime(), "conn_req"]);
+	}).on("end", function(){
+		log.push(["", c, (new Date()).getTime(), "conn_end"]);
+	}).on("error", function(e){
+		log.push([e, c, (new Date()).getTime(), "conn_err"]);
+	});
+}).on("error", function(e){
+	log.push([e, (new Date()).getTime(), "serv_err"]);
+});

2fd98d61334807ee79ef3c065b9c4a623fbb4210
Farhan Sabran <FarhanMS123@users.noreply.github.com>
2017-12-19 23:11:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var bP = require("./bodyParse.js");
 var util = require("util");
 var fs = require("fs");
+var path = require("path");
 
 var app = express();
 app.listen(process.env.PORT);
@@ -10,17 +11,21 @@ app.all("/*", bP, function(req,res,next){
 	if(req.originalUrl.slice(0, ("/test")) == "/test"){
 		res.send(util.inspect(req));
 	}else{
-		var p = "public" = req.path
-		var st = fs.stat(p);
-		if(st.isDirectory()){
-			var d = fs.readdirSync(p);
-			for(i=0;i<d.length;i++){
-				if(d[i].slice(0, d[i].indexOf(".") == "index"){
-				   res.send(fs.readFileSync(p));
+		var p = path.join("public", req.path);
+		if(fs.existsSync(p)){
+			var st = fs.stat(p);
+			if(st.isDirectory()){
+				var d = fs.readdirSync(p);
+				for(i=0;i<d.length;i++){
+					if(d[i].slice(0, d[i].indexOf(".") == "index"){
+					   res.send(fs.readFileSync(p));
+					}
 				}
+			}else{
+				res.send(fs.readFileSync(p));
 			}
 		}else{
-			res.send(fs.readFileSync(p));
+			res.send("404 Not Found :(");
 		}
 	}
 });

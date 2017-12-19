d94d9af2cf2080bc5610ef538ee5731b921cfe67
Farhan Sabran <FarhanMS123@users.noreply.github.com>
2017-12-19 22:45:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var bP = require("./bodyParse.js");
 var util = require("util");
 
 var app = express();
-app.listen(80);
+app.listen(process.env.PORT);
 
 app.all("/*", bP, function(req,res,next){
-	
+	res.send(util.inspect(req));
 });
